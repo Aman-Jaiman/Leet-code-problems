e6ebc0cba13ade05ea1d5039f9188438667803de
Aman-Jaiman <amanjaiman0010@gmail.com>
2026-01-08 19:17:04 +0530

    2 Add Two Number

---
+++ 2_Add_Two_Numbers.cpp
@@ -0,0 +1,82 @@
+/*
+Problem: Add Two Numbers
+
+You are given two non-empty linked lists representing two non-negative integers.
+The digits are stored in reverse order, and each of their nodes contains a single digit.
+
+Add the two numbers and return the sum as a linked list.
+
+You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+
+LeetCode Problem: 2. Add Two Numbers
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// Definition for singly-linked list.
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode* dummy = new ListNode(0);
+        ListNode* curr = dummy;
+        int carry = 0;
+
+        while (l1 || l2 || carry) {
+            int sum = carry;
+
+            if (l1) {
+                sum += l1->val;
+                l1 = l1->next;
+            }
+            if (l2) {
+                sum += l2->val;
+                l2 = l2->next;
+            }
+
+            carry = sum / 10;
+            curr->next = new ListNode(sum % 10);
+            curr = curr->next;
+        }
+
+        return dummy->next;
+    }
+};
+
+int main() {
+    /*
+        Example:
+        l1 = 2 -> 4 -> 3   (represents 342)
+        l2 = 5 -> 6 -> 4   (represents 465)
+
+        Output:
+        7 -> 0 -> 8        (represents 807)
+    */
+
+    ListNode* l1 = new ListNode(2);
+    l1->next = new ListNode(4);
+    l1->next->next = new ListNode(3);
+
+    ListNode* l2 = new ListNode(5);
+    l2->next = new ListNode(6);
+    l2->next->next = new ListNode(4);
+
+    Solution sol;
+    ListNode* result = sol.addTwoNumbers(l1, l2);
+
+    cout << "Result: ";
+    while (result) {
+        cout << result->val << " ";
+        result = result->next;
+    }
+
+    return 0;
+}
