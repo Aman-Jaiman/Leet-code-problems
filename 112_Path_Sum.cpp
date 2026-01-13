0c2fb6f86fe60ccf199722daff8c200962e2cc0f
Aman-Jaiman <amanjaiman0010@gmail.com>
2026-01-13 22:13:17 +0530

    112 Path Sum

---
+++ 112_Path_Sum.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/**
+ * Definition for a binary tree node.
+ */
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right)
+        : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    bool helper(TreeNode* root, int targetSum) {
+        if (root == nullptr) return false;
+
+        // If leaf node, check remaining sum
+        if (!root->left && !root->right)
+            return targetSum == root->val;
+
+        return helper(root->left, targetSum - root->val) ||
+               helper(root->right, targetSum - root->val);
+    }
+
+    bool hasPathSum(TreeNode* root, int targetSum) {
+        return helper(root, targetSum);
+    }
+};
