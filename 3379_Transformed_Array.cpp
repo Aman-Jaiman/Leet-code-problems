0bef9e7afb53196a9fb0966456120a845da5ae04
Aman-Jaiman <amanjaiman0010@gmail.com>
2026-02-06 19:05:58 +0530

    3379. Transformed Array

---
+++ 3379_Transformed_Array.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> circularArrayLoop(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> result(n);
+
+        for (int i = 0; i < n; i++) {
+            if (nums[i] == 0) {
+                result[i] = 0;
+            } else {
+                int newIndex = (i + nums[i]) % n;
+                if (newIndex < 0)
+                    newIndex += n;   // handle negative wrap
+
+                result[i] = nums[newIndex];
+            }
+        }
+        return result;
+    }
+};
