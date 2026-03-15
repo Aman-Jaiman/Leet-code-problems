db584fae822982fad325c88d08398b3421e21cc6
Aman-Jaiman <amanjaiman0010@gmail.com>
2026-03-15 23:33:57 +0530

    796. Rotate String

---
+++ Strings/796_Rotate_String.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+class Solution {
+public:
+    bool rotateString(string s, string goal) {
+        if(s.size() != goal.size()) return false;
+        string temp =s+s;
+        return temp.find(goal)!=string::npos;
+    }
+};
