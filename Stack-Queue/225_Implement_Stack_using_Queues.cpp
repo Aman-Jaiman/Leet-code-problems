8bf8eea3bffee99c37ac3aafa9c98aa5d12fc4c5
Aman-Jaiman <amanjaiman0010@gmail.com>
2026-04-14 00:32:04 +0530

    modified 225. Implement Stack Using Queue

--- Stack-Queue/225_Implement_Stack_using_Queues.cpp
+++ Stack-Queue/225_Implement_Stack_using_Queues.cpp
@@ -1,6 +1,39 @@
 #include <bits/stdc++.h>
 using namespace std;
+//using single queue (optimal)
 
+class MyStack {
+    queue<int> q;
+
+public:
+    MyStack() {}
+
+    void push(int x) {
+        q.push(x);
+        int n = q.size();
+
+        for(int i = 0; i < n - 1; i++) {
+            q.push(q.front());
+            q.pop();
+        }
+    }
+
+    int pop() {
+        int x = q.front();
+        q.pop();
+        return x;
+    }
+
+    int top() {
+        return q.front();
+    }
+
+    bool empty() {
+        return q.empty();
+    }
+};
+
+/*
 class MyStack {
 public:
     queue<int> q1;
@@ -45,6 +78,8 @@ public:
         return q1.empty();
     }
 };
+
+*/
 
 // Driver code (for testing)
 int main() {
