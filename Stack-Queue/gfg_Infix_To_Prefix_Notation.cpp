a82ee0ec519d7ba55b552d4b372813bb946f0779
Aman-Jaiman <amanjaiman0010@gmail.com>
2026-04-21 22:14:18 +0530

    Infix To Prefix Notation

---
+++ Stack-Queue/gfg_Infix_To_Prefix_Notation.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+
+    int prec(char c){
+        if(c=='^') return 3;
+        if(c=='*' || c=='/') return 2;
+        if(c=='+' || c=='-') return 1;
+        return -1;
+    }
+
+    string infixToPrefix(string &s){
+
+        reverse(s.begin(), s.end());
+
+        stack<char> st;
+        string postfix="";
+
+        for(char c : s){
+
+            if(isalnum(c))
+                postfix += c;
+
+            else if(c==')')
+                st.push(c);
+
+            else if(c=='('){
+                while(!st.empty() && st.top()!=')'){
+                    postfix += st.top();
+                    st.pop();
+                }
+                if(!st.empty()) st.pop();
+            }
+
+            else{
+                while(!st.empty() &&
+                      (prec(st.top()) > prec(c) ||
+                      (prec(st.top()) == prec(c) && c=='^'))){
+                    
+                    postfix += st.top();
+                    st.pop();
+                }
+
+                st.push(c);
+            }
+        }
+
+        while(!st.empty()){
+            postfix += st.top();
+            st.pop();
+        }
+
+        reverse(postfix.begin(), postfix.end());
+
+        return postfix;
+    }
+};
