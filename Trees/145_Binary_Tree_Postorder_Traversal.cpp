382e89ebd170fd6f84aa138112aec10eee59f969
Aman-Jaiman <amanjaiman0010@gmail.com>
2026-05-07 23:47:59 +0530

    itrative approch

--- Trees/145_Binary_Tree_Postorder_Traversal.cpp
+++ Trees/145_Binary_Tree_Postorder_Traversal.cpp
@@ -15,6 +15,7 @@ struct TreeNode {
         : val(x), left(left), right(right) {}
 };
 
+//recusive approch
 class Solution {
 public:
     vector<int> ans;
@@ -30,6 +31,30 @@ public:
 
     vector<int> postorderTraversal(TreeNode* root) {
         postOrder(root);
+        return ans;
+    }
+};
+
+//itative approch using 2 stack --- stack1 to store root and stack2 store poped elemet from stack1 to maintain reverse order push Root in stack1 , pop root and push in stack2 and at same time push element in stack1 Left and Right of the Root   
+
+
+// iterative approch using single stack --- same as 2 stack but insted of second stack we will use array reversing
+class Solution {
+public:
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int>ans;
+        if(!root) return ans;
+        stack<TreeNode*>st;
+        stack<TreeNode*>st2;
+        st.push(root);
+        while(!st.empty()){
+            TreeNode* node =st.top();
+            ans.push_back(node->val);
+            st.pop();
+            if(node->left) st.push(node->left);
+            if(node->right) st.push(node->right);
+        }
+        reverse(ans.begin(),ans.end());
         return ans;
     }
 };
