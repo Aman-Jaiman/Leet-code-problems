db9c17fc7836f3c6543dd062a55dafc83957cc5e
Aman-Jaiman <amanjaiman0010@gmail.com>
2026-06-10 13:28:05 +0530

    Frog Jump

---
+++ Dynamic_Programming/gfg_Frog_Jump.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// Using Memorization --> Tabulation (down --> top)
+// TC: O(n)  and SC: O(n)
+class Solution {
+  public:
+    int minCost(vector<int>& height) {
+        int n=height.size();
+        vector<int>dp(n,-1);
+        dp[0]=0;
+        for(int i=1;i<n;i++) {
+            int left= dp[i-1]+abs(height[i]-height[i-1]);
+            int right=INT_MAX;
+            if(i>1) {
+                right= dp[i-2]+abs(height[i]-height[i-2]);
+            }
+            dp[i]=min(left,right);
+        }
+        return dp[n-1];
+    }
+};
+
+// Using Recursion --> Memorization (top --> down)
+// TC: O(n) and SC: O(n)+O(n)
+class Solution {
+  public:
+    int helper(int i, vector<int>& height, vector<int>&dp){
+        if(i==0) return 0;
+        if(dp[i]!=-1) return dp[i];
+        int left= helper(i-1,height,dp)+abs(height[i]-height[i-1]);
+        int right=INT_MAX;
+        if(i>1){
+            right=helper(i-2,height,dp)+abs(height[i]-height[i-2]);
+        }
+        return dp[i] = min(left,right);
+    }
+    int minCost(vector<int>& height) {
+        // Code here
+        int n=height.size();
+        vector<int>dp(n,-1);
+        return helper(n - 1, height, dp);
+    }
+};
