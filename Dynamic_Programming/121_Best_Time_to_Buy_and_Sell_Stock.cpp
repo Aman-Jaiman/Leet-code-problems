26db2dd31f514927cd836dcdfa53564db033085a
Aman-Jaiman <amanjaiman0010@gmail.com>
2026-07-17 18:37:26 +0530

    121. Best Time to Buy and Sell Stock

---
+++ Dynamic_Programming/121_Best_Time_to_Buy_and_Sell_Stock.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int mini = prices[0];
+        int ans=0;
+        for(int i=1; i<prices.size(); i++){
+            ans = max(ans, prices[i] - mini);
+
+            // Remembring is Part of dp
+            mini = min(mini, prices[i]);
+        }
+        return ans;
+    }
+};
