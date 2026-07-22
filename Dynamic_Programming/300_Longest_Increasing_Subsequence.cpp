d3067895b8ff3621725f2e2a165ac70c45ab9b05
Aman-Jaiman <amanjaiman0010@gmail.com>
2026-07-22 18:59:25 +0530

    more approches

--- Dynamic_Programming/300_Longest_Increasing_Subsequence.cpp
+++ Dynamic_Programming/300_Longest_Increasing_Subsequence.cpp
@@ -1,6 +1,71 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+// memorization
+class Solution {
+public:
+
+    int f(int i , int prev, vector<int> &nums, vector<vector<int>> &dp){
+
+        if(i == nums.size()) return 0;
+
+        if(dp[i][prev +1] != -1) dp[i][prev +1];
+
+        // not Take ;
+        int len = 0 + f(i+1, prev, nums, dp);
+
+        if(prev == -1 || nums[i] > nums[prev]){
+            return dp[i][prev+1] = max(len, 1 + f(i+1, i, nums, dp));
+        }
+        return dp[i][prev+1] = len;
+    }
+     
+    int lengthOfLIS(vector<int>& nums) {
+        int n = nums.size();
+        vector<vector<int>> dp(n, vector<int> (n +1, -1));
+        return f(0, -1, nums, dp);
+    }
+};
+
+// better solution 
+//tabulation without extra calling space
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+
+        int n = nums.size();
+
+        // dp[i][prev+1]
+        vector<vector<int>> dp(n + 1, vector<int>(n + 1, 0));
+
+        // Base case:
+        // dp[n][*] = 0 (already initialized)
+
+        for (int i = n - 1; i >= 0; i--) {
+
+            // prev can only be -1 to i-1
+            for (int prev = i - 1; prev >= -1; prev--) {
+
+                // Not Take
+                int notTake = dp[i + 1][prev + 1];
+
+                // Take
+                int take = 0;
+                if (prev == -1 || nums[i] > nums[prev]) {
+                    take = 1 + dp[i + 1][i + 1];
+                }
+
+                dp[i][prev + 1] = max(take, notTake);
+            }
+        }
+
+        return dp[0][0];
+    }
+};
+
+
+// optimal soution
+// O(n) time
 class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
@@ -24,7 +89,7 @@ public:
                         lb = mid + 1;
                 }
 
-                arr[lb] = x;   // ✅ Replace after binary search finishes
+                arr[lb] = x;  
             }
         }
 
